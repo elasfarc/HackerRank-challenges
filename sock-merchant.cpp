5eae4f411649e6022aa520d0ad0458afb26d5583
elasfarc <63879921+elasfarc@users.noreply.github.com>
2020-05-27 11:58:32 +0200

    0.0

---
+++ sock-merchant.cpp
@@ -0,0 +1,29 @@
+// Complete the sockMerchant function below.
+// calculate how many pieces of each @color and store it @eachColorCounter
+int sockMerchant(int n, vector<int> ar) {
+
+    int numPairs = 0;
+    int eachColorCounter = 0;
+    for(int i = 0; i < ar.size(); ++i){
+
+      if(ar[i] != 0){  // ar[i] == 0 -> @color already counted
+        int color = ar[i];
+
+        for(int j = 0; j < ar.size() ; j++){
+
+          if(color == ar[j]  ) {
+            eachColorCounter+=1;
+            ar[j] = 0;
+          }
+
+        }//ineer loop
+
+        numPairs += eachColorCounter / 2;
+        eachColorCounter = 0;
+
+      }
+    }//outer loop
+
+   return numPairs;
+
+}
